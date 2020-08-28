176c984a366279e5dfdffe3e294970e80ecbc80a
phoenixfate <sm516116978@outlook.com>
2020-08-28 10:04:24 +0800

    jpql

--- spring-data-jpa/src/main/java/com/phoenix/dao/CustomerDao.java
+++ spring-data-jpa/src/main/java/com/phoenix/dao/CustomerDao.java
@@ -3,6 +3,8 @@ package com.phoenix.dao;
 import com.phoenix.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * SpringDataJpa dao层的接口规范
@@ -15,5 +17,40 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 public interface CustomerDao extends JpaRepository<Customer,Long>, JpaSpecificationExecutor<Customer> {
 
 
+    /**
+     * 使用jpql需要将jpaql语句配置到接口方法上
+     *      1.特有的查询：需要在dao接口上配置方法
+     *      2.在新添加的方法上，使用注解的形式配置jpql查询语句
+     *      3.注解：@Query
+     * jpql: from Customer where custName = ?
+     */
+    @Query(value = "from Customer where custName = ?1")
+    Customer findByNameByJpql(String name);
+
+    /**
+     * 对于多个占位符参数
+     *      赋值的时候，默认的情况下，占位符的位置需要和方法参数中的位置保持一致
+     * 可以指定占位符参数的位置
+     *      ？ 索引的方式，指定此占位的取值来源
+     * @param name
+     * @param id
+     * @return
+     */
+    @Query("from Customer where custName = ?1 and custId=?2")
+    Customer findByNameAndIdByJpql(String name,Long id);
+
+
+    /**
+     * jpql update
+     *
+     * @Query声明当前sql、jpql是一个查询操作
+     * @Modifying声明当前sql、jpql是一个更新操作
+     *
+     * @param name
+     * @param id
+     */
+    @Query("update Customer set custName=?1 where custId=?2")
+    @Modifying
+    void updateCustomerName(String name,Long id);
 
 }
